ba8cb1efb98ba3d8c7551cfde5e09b7355042d5f
Matt Williams <matwilliams@hotmail.com>
2017-04-23 10:37:28 +0100

    Add Bluetooth handling

--- beacon/index.js
+++ beacon/index.js
@@ -3,10 +3,41 @@ var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 var MyContract = web3.eth.contract(require('./abi.json'));
 
+var tokens = [];
+var crypto = require('crypto');
+var child_process = require('child_process');
+var util = require('util');
+var localIp = require('ip').toBuffer(require('ip').address()).toString("hex");
+
+function refreshToken() {
+  crypto.randomBytes(8, function(err, buffer) {
+    if (err) {
+      console.log("Failed to get random token");
+      setTimeout(refreshToken, 10000);
+    } else {
+      var token = buffer.toString('hex');
+      tokens.splice(0, Math.max(tokens.length - 2, 0));
+      tokens.push(token);
+      console.log("Tokens now", tokens);
+      var cmd = "hcitool -i hci0 cmd 0x08 0x0008 0c 0b ff 52 4d " + localIp.replace(/../g, function(x) {return x + " "}) + token.replace(/../g, function(x) {return x + " "});
+      child_process.exec(cmd, function(err, stdout, stderr) {
+        if (err) {
+          console.log("Updating Bluetooth failed");
+          console.log("  Command:", cmd);
+          console.log("  stdout:", stdout);
+          console.log("  stderr:", stderr);
+        }
+        setTimeout(refreshToken, 10000);
+      });
+    }
+  });
+}
+refreshToken();
+
 var express = require('express');
 var app = express()
 
-app.get('/contracts/:contract', function (req, res) {
+app.get('/contracts/:contract', function(req, res) {
   var contract = req.params.contract;
   if (contract) {
     db.get("SELECT account FROM contracts where contract = ?", contract, function(err, row) {
@@ -43,9 +74,10 @@ app.get('/contracts/:contract', function (req, res) {
   }
 });
 
-app.post('/contracts/:contract', function (req, res) {
+app.post('/contracts/:contract', function(req, res) {
   var contract = req.params.contract;
   var account = req.query.account;
+  var token = req.query.token; // TODO Check this
   if (contract && account) {
     db.get("SELECT account FROM contracts where contract = ?", contract, function(err, row) {
       if (err) {
